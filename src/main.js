ba24b48dafac600100d7ac4738b52af04f9078fe
Виктория Зайкина <viktoriazajkina07842@mail.ru>
2026-07-12 21:47:18 +0500

    Настройка сборки и базовая разметка

---
+++ src/main.js
@@ -0,0 +1,3 @@
+import './styles/style.css';
+
+console.log('Weather Assistant started');
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
